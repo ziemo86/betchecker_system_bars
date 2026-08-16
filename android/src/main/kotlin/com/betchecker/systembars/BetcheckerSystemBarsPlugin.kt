a85e07ef3933f64c1a774a2b54659988253c5518
ziemo86 <ziemo86@gmail.com>
2026-08-16 14:30:37 +0200

    Create BetcheckerSystemBarsPlugin.kt

---
+++ android/src/main/kotlin/com/betchecker/systembars/BetcheckerSystemBarsPlugin.kt
@@ -0,0 +1,33 @@
+package com.betchecker.systembars
+
+import android.os.Build
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+
+class BetcheckerSystemBarsPlugin : FlutterPlugin, ActivityAware {
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            binding.activity.window.isNavigationBarContrastEnforced = false
+        }
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            binding.activity.window.isNavigationBarContrastEnforced = false
+        }
+    }
+
+    override fun onDetachedFromActivity() {
+    }
+}
